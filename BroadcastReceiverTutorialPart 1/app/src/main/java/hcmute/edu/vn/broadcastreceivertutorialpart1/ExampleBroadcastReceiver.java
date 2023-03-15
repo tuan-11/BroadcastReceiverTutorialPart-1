5f8a4ab3b05a5bf0f54a9ec661721c75bd68c8db
Trần Quốc Tuấn <tranquoctuantqd2002@gmail.com>
2023-03-15 10:53:56 +0700

    update BroadcastReceiverTutorialPart-1

---
+++ BroadcastReceiverTutorialPart 1/app/src/main/java/hcmute/edu/vn/broadcastreceivertutorialpart1/ExampleBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package hcmute.edu.vn.broadcastreceivertutorialpart1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+            Toast.makeText(context, "Boot completed",Toast.LENGTH_LONG).show();
+        }
+
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            Toast.makeText(context, "Connectivity changed",Toast.LENGTH_LONG).show();
+        }
+    }
+}
